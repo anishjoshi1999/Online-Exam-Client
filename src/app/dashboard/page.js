721507e436a6fb8b5f0558c3d23584a16814fcaf
anishjoshi1999 <anishjoshi1999@gmail.com>
2025-01-22 19:55:32 +0545

    hide non-admin card

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -144,7 +144,7 @@ function Dashboard() {
             </div>
           </div>
         </div>
-        {!isAdmin && (
+        {/* {!isAdmin && (
           <div className="bg-white rounded-lg shadow-sm border border-gray-100 p-6 mb-8">
             <h2 className="text-2xl font-semibold text-gray-800 mb-2">
               Interested in Conducting Online MCQ Exams?
@@ -171,7 +171,7 @@ function Dashboard() {
               </Link>
             </div>
           </div>
-        )}
+        )} */}
         {/* Stats Grid */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-8">
           {/* System Status Card */}
